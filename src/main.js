d47f273fb85d0c79a63c4bd985199d4556a6e613
rferro <ricardo.ferro@gmail.com>
2016-04-05 23:23:31 -0300

    main.js improvements

--- src/main.js
+++ src/main.js
@@ -71,23 +71,6 @@
 
     $after.fadeIn('fast');
     $before.slideUp('fast');
-
-    $('.ms').click(function () {
-      var range, selection;
-
-      if (window.getSelection) {
-         selection = window.getSelection();
-         range = document.createRange();
-         range.selectNodeContents(this);
-         selection.removeAllRanges();
-         selection.addRange(range);
-      }
-      else if (document.body.createTextRange) {
-         range = document.body.createTextRange();
-         range.moveToElementText(this);
-         range.select();
-      }
-    });
   }
 
   var setSecret = function (data) {
@@ -152,4 +135,20 @@
     }
   });
 
+  $('.hash').click(function () {
+    var range, selection;
+
+    if (window.getSelection) {
+       selection = window.getSelection();
+       range = document.createRange();
+       range.selectNodeContents(this);
+       selection.removeAllRanges();
+       selection.addRange(range);
+    }
+    else if (document.body.createTextRange) {
+       range = document.body.createTextRange();
+       range.moveToElementText(this);
+       range.select();
+    }
+  });
 })(jQuery)
