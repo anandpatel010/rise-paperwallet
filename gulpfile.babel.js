4e84ff1211210cc0415e04ee7cbbb7c436de037d
spookiestevie <nandopatel2001@yahoo.co.uk>
2019-05-27 18:03:35 +0100

    dependency mngmt

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,33 +8,33 @@ import jade from 'gulp-jade'
 
 gulp.task('babel', () => {
   gulp
-    .src(['src/**/*.js', '!src/wallet.js'])
-    .pipe(watch('src/**/*.js', { verbose: true }))
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(gulp.dest('lib'))
+      .src(['src/**/*.js', '!src/wallet.js'])
+      .pipe(watch('src/**/*.js', { verbose: true }))
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(gulp.dest('lib'))
 })
 
 gulp.task('less', () => {
   gulp
-    .src('src/**/*.less')
-    .pipe(watch('src/**/*.less'))
-    .pipe(less())
-    .pipe(gulp.dest('lib'))
+      .src('src/**/*.less')
+      .pipe(watch('src/**/*.less'))
+      .pipe(less())
+      .pipe(gulp.dest('lib'))
 })
 
 gulp.task('jade', () => {
   gulp
-    .src('src/**/*.jade')
-    .pipe(watch('src/**/*.jade'))
-    .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('.'))
+      .src('src/**/*.jade')
+      .pipe(watch('src/**/*.jade'))
+      .pipe(jade({ pretty: true }))
+      .pipe(gulp.dest('.'))
 })
 
 gulp.task('bundle', () => {
   gulp
-    .src('src/wallet.js')
-    .pipe(watch('src/wallet.js', { verbose: true }))
-    .pipe(babel({ presets: ['es2015'] }))
-    .pipe(gulpBrowser.browserify())
-    .pipe(gulp.dest('lib'))
+      .src('src/wallet.js')
+      .pipe(watch('src/wallet.js', { verbose: true }))
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(gulpBrowser.browserify())
+      .pipe(gulp.dest('lib'))
 })
